87dc4e182c94914b0c4f629e0d68db5f2a0c2be1
stevenslcc2430 <sbenjam2@bruinmail.slcc.edu>
2026-02-17 17:10:24 -0700

    Update Part1_OrdinarySets.py

--- src/Part1_OrdinarySets.py
+++ src/Part1_OrdinarySets.py
@@ -1,1 +1,11 @@
+import random
 
+class Part1_OrdinarySets:
+
+	universal = ["Apple", "Banana", "Milk", "Bread", "Eggs", "Cheese", "Pasta", "Yogurt", "Tea", "Oatmeal"]
+	n = len(universal)
+
+	# random.choice to create subsets of universal set
+	bool_a = [random.choice([True, False]) for _ in range(n)]
+	bool_b = [random.choice([True, False]) for _ in range(n)]
+  
