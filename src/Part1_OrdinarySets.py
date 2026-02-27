28aa237d16993242ece9c832f8e729c78af22a49
Alexg11111 <141282391+Alexg11111@users.noreply.github.com>
2026-02-26 21:02:12 -0700

    Update Part1_OrdinarySets.py
    Had to change and add a 2nd version of the set operations/Sets since changing the values of set A/B to empty(all False) after creating an instance of Part1_OrdinarySets does not redo the operations. Basically The operations would hold the results of the original operations done with the randomly generated values and not the empty set values. Found this after 20 mins of testing

--- src/Part1_OrdinarySets.py
+++ src/Part1_OrdinarySets.py
@@ -8,7 +8,10 @@ class Part1_OrdinarySets:
         # Create random boolean lists for two subsets
         self.bool_a = [random.choice([True, False]) for _ in range(self.n)]
         self.bool_b = [random.choice([True, False]) for _ in range(self.n)]
-
+        #empty sets
+        self.empty_testA = [False] * self.n
+        self.empty_testB = [False] * self.n
+        #normal sets
         A = self.bool_a
         B = self.bool_b
 
@@ -17,31 +20,37 @@ class Part1_OrdinarySets:
         # Uses the function get_set_from_boolean to display output
         # Creates set by checking which value is false in set A(bool_a) and adding it to bool_complement
         self.complement_a = [not value for value in A]
-
+        #empty set
+        self.complement_aEmpty = [not value for value in self.empty_testA]
         # Union Operation
         # Returns the union A and B
         # Uses the function get_set_from_boolean to display output
         # Combines both sets into bool_union, then whichever values are true are displayed while false values are left undisplayed (maybe problem keeping false values in union set?)
         self.union = [A[i] or B[i] for i in range(self.n)]
-
+        #empty set
+        self.unionEmpty = [self.empty_testA[i] or self.empty_testB[i] for i in range(self.n)]
         # Intersection operation
         # Return the intersection of A and B
         # Uses the function get_set_from_boolean to display output
         # Creates set bool_intersection by checking if both values in current loop are true, if true then add to bool_intersection else leave out
         self.intersection = [A[i] and B[i] for i in range(self.n)]
-
+        #empty
+        self.intersectionEmpty = [self.empty_testA[i] and self.empty_testB[i] for i in range(self.n)]
         # Difference operation
         # Return A - B
         # uses the function get_set_from_boolean to display output
         # creates set bool_difference by checking if set a value is true and checking if set b value is false
         self.difference_a_minus_b = [A[i] and not B[i] for i in range(self.n)]
+        #empty
+        self.difference_a_minus_bEmpty = [self.empty_testA[i] and not self.empty_testB[i] for i in range(self.n)]
 
         # Symmetric difference
         # Return symmetric difference (elements in A or B but not in both)
         # uses the function get_set_from_boolean to display output
-        # creats bool_sym_set by using the XOR(^) operator to check that only 1 set has a true value and the other set has a false value which makes the element unique
+        # creates bool_sym_set by using the XOR(^) operator to check that only 1 set has a true value and the other set has a false value which makes the element unique
         self.symmetric_difference = [A[i] ^ B[i] for i in range(self.n)]  # XOR operator
-
+        #empty set
+        self.symmetric_differenceEmpty = [self.empty_testA[i] ^ self.empty_testB[i] for i in range(self.n)]
     def get_set_from_boolean(self, bool_list):
         # Convert boolean values to actual set of items by returning any true value from the random boolean list using a for loop
         return [self.universal[i] for i in range(self.n) if bool_list[i]]
@@ -62,25 +71,6 @@ class Part1_OrdinarySets:
             f"{'A - B - Difference':<25} | {list(map(int, self.difference_a_minus_b))} | {self.get_set_from_boolean(self.difference_a_minus_b)}")
         print(
             f"{'A ⊕ B - Symm Diff':<25} | {list(map(int, self.symmetric_difference))} | {self.get_set_from_boolean(self.symmetric_difference)}")
-
-class empty_test:
-    def __init__(self):
-        #empty set
-        self.empty1 = []
-        self.empty2 = []
-        self.n = len(self.empty1)
-        #operations
-        # Return the complement of set A (everything in universal but not in A)
-        self.complement_a = [not value for value in self.empty1]
-        # Union Operation
-        # Returns the union A and B
-        self.union = [self.empty1[i] or self.empty[i] for i in range(self.n)]
-        # Intersection operation
-        self.intersection = [self.empty1[i] and self.empty2[i] for i in range(self.n)]
-        # Difference operation
-        self.difference_a_minus_b = [self.empty1[i] and not self.empty2[i] for i in range(self.n)]
-        # Symmetric difference
-        self.symmetric_difference = [self.empty1[i] ^ self.empty2[i] for i in range(self.n)]  # XOR operator
 
 # Example usage
 if __name__ == "__main__":
