49c681d10cff1e84a14490b4d04b2438cc4d0e70
stevenslcc2430 <sbenjam2@bruinmail.slcc.edu>
2026-03-04 02:35:05 -0700

    Update main.py
    added argparser

--- src/main.py
+++ src/main.py
@@ -1,1 +1,21 @@
+import argparse
+from collections import Counter
 
+# Setup the CLI argparser 
+parser = argparse.ArgumentParser(description="Set vs Multiset Project")
+parser.add_argument("--select", choices=["set", "bag"], required=True, help="Choose 'set' for Bit Strings or 'bag' for Multisets")
+
+args = parser.parse_args()
+
+# 2. Routing Logic
+if args.mode == "set":
+    print("Running Part 1: Ordinary Sets - Bit strings")
+    sets = Part1_OrdinarySets()
+    # Display the original sets
+    sets.display_sets()
+    
+elif args.mode == "bag":
+    print("Running Part 2: Multisets")
+    multisets = Part2_Multisets()
+    # Display multiset operations
+    multisets.display_bags()
